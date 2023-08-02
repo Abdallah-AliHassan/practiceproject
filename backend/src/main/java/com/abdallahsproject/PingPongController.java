f038888aec68c7d0dd5ee0ae568a4548a539bc28
Abdallah-AliHassan <abdallah.ali22596@gmail.com>
2023-08-02 17:42:12 +0200

    CD workflow test

---
+++ backend/src/main/java/com/abdallahsproject/PingPongController.java
@@ -0,0 +1,14 @@
+package com.abdallahsproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+    record PingPong(String res){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+}
